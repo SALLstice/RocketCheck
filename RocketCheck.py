ffe430d2c25551bf327d0041ab2ed3d29e1179bb
SALLstice <MDSall7@gmail.com>
2020-02-07 12:13:32 -0600

    notifications

--- RocketCheck.py
+++ RocketCheck.py
@@ -1,6 +1,7 @@
 import win32com.client
 import os
 import pythoncom
+from win10toast import ToastNotifier
 
 class Handler_Class(object):
   def OnNewMailEx(self, receivedItemsIDs):
@@ -15,16 +16,18 @@ class Handler_Class(object):
                 if attachment.FileName[-3:] == "txt":
                     with open(os.path.join(path, str(attachment.FileName))) as file:
                         for line in file:
-                            if "TODO:" in line:
+                            if "TODO" in line:
                                 newtask = outlook.CreateItem(win32com.client.constants.olTaskItem)
                                 newtask.Subject = line
                                 pdfpath = os.path.join(path, str(attachment.FileName)[:-23]+".pdf")
                                 newtask.Attachments.Add(pdfpath)
                                 newtask.Save()
+                                toaster.show_toast("New Rocketbook TODO", line)
 
 outlook = win32com.client.DispatchWithEvents("Outlook.Application", Handler_Class)
 mapi = outlook.GetNamespace("MAPI")
 path = r'L:\Rocketbook Scans'
+toaster = ToastNotifier()
 
 print("Running...")
 pythoncom.PumpMessages()
