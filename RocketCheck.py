007e762d9ae39ce4a57e62b5b89bc67e6757297c
Matt Sall <MDSall7@gmail.com>
2020-02-24 10:06:45 -0600

    Checks folder on run

--- RocketCheck.py
+++ RocketCheck.py
@@ -9,45 +9,47 @@ class Handler_Class(object):
   def OnNewMailEx(self, receivedItemsIDs):
     for ID in receivedItemsIDs.split(","):
         message = mapi.Session.GetItemFromID(ID)
-        if message.SenderEmailAddress == "notes@email.getrocketbook.com":
-            print(f"New Message Found: {message.Subject}")
-            toaster.show_toast(f"New Rocketbook Message",message.Subject)
-            for attachment in message.Attachments:
-                attachment.SaveAsFile(os.path.join(path, str(attachment.FileName)))
-                message.UnRead = False
+        doTheThing(message)
 
-                if attachment.FileName[-3:] == "txt":
-                    with open(os.path.join(path, str(attachment.FileName))) as file:
-                        for line in file:
-                            if "TODO" in line:
-                                newtask = outlook.CreateItem(win32com.client.constants.olTaskItem)
-                                newtask.StartDate = pywintypes.Time(date.today())
+def doTheThing(message):
+    if message.SenderEmailAddress == "notes@email.getrocketbook.com":
+        print(f"New Rocketbook Message: {message.Subject}")
+        toaster.show_toast(f"New Rocketbook Message",message.Subject)
+        for attachment in message.Attachments:
+            attachment.SaveAsFile(os.path.join(path, str(attachment.FileName)))
+            message.UnRead = False
 
-                                #sets due date to yesterday if todo is high priority, to highlight red
-                                if "!TODO" in line:
-                                    newtask.DueDate = pywintypes.Time(date.today() - timedelta(days=1))
-                                else:
-                                    newtask.DueDate = pywintypes.Time(date.today())
+            if attachment.FileName[-3:] == "txt":
+                with open(os.path.join(path, str(attachment.FileName))) as file:
+                    for line in file:
 
-                                if "xTODO" in line or "XTODO" in line:
-                                    newtask.DateCompleted = pywintypes.Time(date.today())
+                        #adds TODOs but ignores xTODOs
+                        if "TODO" in line and not ("xTODO" in line or "XTODO" in line):
+                            newtask = outlook.CreateItem(win32com.client.constants.olTaskItem)
+                            newtask.StartDate = pywintypes.Time(date.today())
+                            newtask.DueDate = pywintypes.Time(date.today())
 
-                                #trims from front of string until finds TODO
-                                TODOCount = 0
-                                while TODOCount < 4:
-                                    if line[0] in ["T", "O", "D"]:
-                                        TODOCount += 1
-                                    line = line[1:]
+                            #sets priority to high of !TODOs
+                            if "!TODO" in line:
+                                newtask.Importance = 2
 
-                                # keeps trimming from front until an alphanumeric character is found
-                                while not (line[0].isalnum()):
-                                    line = line[1:]
+                            #trims from front of string until finds TODO
+                            TODOCount = 0
+                            while TODOCount < 4:
+                                if line[0] in ["T", "O", "D"]:
+                                    TODOCount += 1
+                                line = line[1:]
 
-                                # sets subject to TODO line and attaches pdf of RB scan
-                                newtask.Subject = line
-                                pdfpath = os.path.join(path, str(attachment.FileName)[:-23] + ".pdf")
-                                newtask.Attachments.Add(pdfpath)
-                                newtask.Save()
+                            # keeps trimming from front until an alphanumeric character is found
+                            while not (line[0].isalnum()):
+                                line = line[1:]
+
+                            # sets subject to TODOs line and attaches pdf of RB scan
+                            newtask.Subject = line
+                            pdfpath = os.path.join(path, str(attachment.FileName)[:-23] + ".pdf")
+                            newtask.Attachments.Add(pdfpath)
+                            newtask.Save()
+                            print(f"New TODO Created: {newtask.Subject}")
 
 outlook = win32com.client.DispatchWithEvents("Outlook.Application", Handler_Class)
 mapi = outlook.GetNamespace("MAPI")
@@ -55,4 +57,13 @@ path = r'L:\Rocketbook Scans'
 toaster = ToastNotifier()
 
 print("Running...")
-pythoncom.PumpMessages()
+
+inbox = mapi.GetDefaultFolder(6)
+messages = inbox.Folders["Scanned Docs"].Folders["Rocketbook"].Items
+
+for message in messages:
+    if message.UnRead:
+        doTheThing(message)
+
+while True:
+    pythoncom.PumpMessages()
